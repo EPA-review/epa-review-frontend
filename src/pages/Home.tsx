55478e2191bccbe4fee89acdb0ee55960249cde2
Sean Wong <seanwong24@live.com>
2021-07-06 23:35:22 -0600

    remove sign up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,17 +11,12 @@ import styles from './Home.module.css';
 
 const Home: React.FC = () => {
   const history = useHistory();
-  const [isSigningUp, setIsSigningUp] = useState<boolean>(false);
   const [userInfo, setUserInfo] = useState<User>({});
   const [presentAlert] = useIonAlert();
 
   const inputs = [
     { name: 'username', label: 'Username', type: 'text', require: true },
     { name: 'password', label: 'Password', type: 'password', require: true },
-    { name: 'nickname', label: 'Nickname', type: 'text', require: false, isSigningUpOnly: true },
-    { name: 'firstName', label: 'First Name', type: 'text', require: false, isSigningUpOnly: true },
-    { name: 'middleName', label: 'Middle Name', type: 'text', require: false, isSigningUpOnly: true },
-    { name: 'lastName', label: 'Last Name', type: 'text', require: false, isSigningUpOnly: true },
   ];
 
   useEffect(() => {
@@ -48,36 +43,29 @@ const Home: React.FC = () => {
             </IonCardHeader>
             <form onSubmit={event => {
               event.preventDefault();
-              isSigningUp ? signUp(userInfo, presentAlert, setUserInfo, setIsSigningUp) : signIn(userInfo, history, presentAlert, setUserInfo);
+              signIn(userInfo, history, presentAlert, setUserInfo);
             }}>
               {
-                inputs
-                  .filter(input => !input.isSigningUpOnly || isSigningUp)
-                  .map((input, i) => (
-                    <IonItem key={i}>
-                      <IonLabel position="floating">{input.label}</IonLabel>
-                      <IonInput
-                        type={input.type as any} // TODO use specific type
-                        name={input.name}
-                        required={input.require}
-                        onIonChange={
-                          async ({ detail }) => input.name === 'password' ?
-                            userInfo.authenticationHash = await digestText(detail.value?.toString()) :
-                            (userInfo as any)[input.name] = detail.value
-                        }
-                      ></IonInput>
-                    </IonItem>
-                  ))
+                inputs.map((input, i) => (
+                  <IonItem key={i}>
+                    <IonLabel position="floating">{input.label}</IonLabel>
+                    <IonInput
+                      type={input.type as any} // TODO use specific type
+                      name={input.name}
+                      required={input.require}
+                      onIonChange={
+                        async ({ detail }) => input.name === 'password' ?
+                          userInfo.authenticationHash = await digestText(detail.value?.toString()) :
+                          (userInfo as any)[input.name] = detail.value
+                      }
+                    ></IonInput>
+                  </IonItem>
+                ))
               }
               <IonButton
                 type="submit"
                 expand="block"
-              >{isSigningUp ? 'Sign Up' : 'Sign in'}</IonButton>
-              <IonButton
-                expand="block"
-                fill="outline"
-                onClick={() => toggleSignInOrUp(isSigningUp, setIsSigningUp)}
-              >{isSigningUp ? 'Sign In' : 'Sign Up'}</IonButton>
+              >Sign in</IonButton>
             </form>
           </IonCardContent>
         </IonCard>
@@ -114,52 +102,6 @@ async function signIn(
     presentAlert('Fail to sign in.', [{ text: 'OK' }]);
     setUserInfo({});
   }
-}
-
-async function signUp(
-  userInfo: User,
-  presentAlert: { (message: string, buttons?: AlertButton[] | undefined): void; (options: AlertOptions & HookOverlayOptions): void; },
-  setUserInfo: (userInfo: User) => void,
-  setIsSigningUp: (isSigningUp: boolean) => void
-) {
-  const response = await fetch(
-    `${ServerInfo.SERVER_BASE_URL}/authentication/sign-up`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: userInfo.username,
-        authenticationHash: userInfo.authenticationHash,
-        nickname: userInfo.nickname,
-        firstName: userInfo.firstName,
-        middleName: userInfo.middleName,
-        lastName: userInfo.lastName
-      })
-    }
-  );
-  if (response.ok) {
-    presentAlert(
-      'Success to sign up, please sign in with the new account.',
-      [
-        {
-          text: 'OK',
-          handler: () => setIsSigningUp(false)
-        }
-      ]
-    );
-    setUserInfo({});
-  } else {
-    presentAlert('Fail to sign up.', [{ text: 'OK' }]);
-  }
-}
-
-function toggleSignInOrUp(
-  isSigningUp: boolean,
-  setIsSigningUp: (isSigningUp: boolean) => void
-) {
-  setIsSigningUp(!isSigningUp);
 }
 
 async function digestText(text?: string) {
