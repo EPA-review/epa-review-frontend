c7688a22545e7a4854d0ca84adfb5d1de5a93d3c
Sean Wong <seanwong24@live.com>
2021-07-13 15:15:19 -0600

    implement user actions in review detail page

--- src/pages/ReviewDetail.tsx
+++ src/pages/ReviewDetail.tsx
@@ -7,11 +7,19 @@ import ServerInfo from '../utils/ServerInfo';
 
 import styles from './ReviewDetail.module.css';
 
-type EpaFeedback = { originalText: string, tags: { start: number, end: number, name: string }[] };
+type EpaFeedback = {
+  originalText: string,
+  tags: { start: number, end: number, name: string }[],
+  shouldReplaceTextsWithTags: boolean,
+  isEditing: boolean,
+  hasApproved: boolean,
+  clickHandler: (event: Event) => void;
+};
 
 const Dashboard: React.FC = () => {
   const { groupTag } = useParams<{ groupTag: string }>();
   const [data, setData] = useState<EpaFeedback[]>();
+  const [, forceUpdate] = useState({});
   const [presentUserMenuPopover, dismissUserMenuPopover] = useIonPopover(UserMenu, { onHide: () => dismissUserMenuPopover() });
 
   useEffect(() => {
@@ -56,38 +64,98 @@ const Dashboard: React.FC = () => {
         <IonGrid>
           {
             data ?
-              data.map(({ originalText, tags }, i) => (
-                <IonRow key={i}>
-                  <IonCol>
-                    <IonCard className={styles.card}>
-                      <IonCardContent>
-                        <s-magic-text ref={el => {
-                          if (el) {
-                            el.text = originalText;
-                            el.highlights = tags?.map(tag => ({ ...tag, tag: tag.name, style: { color: 'lightblue' } }));
-                            el.addEventListener('segmentClick', ({ detail }: any) => {
-                              alert('a dialog with actions should be presented.');
-                            });
-                          }
-                        }}></s-magic-text>
-                      </IonCardContent>
-                    </IonCard>
-                  </IonCol>
-                  <IonCol size="auto">
-                    <IonCard className={styles.card} style={{ width: '192px' }}>
-                      <IonButton color="success" fill="outline" title="Approve">
-                        <IonIcon slot="icon-only" icon={checkmark}></IonIcon>
-                      </IonButton>
-                      <IonButton color="warning" fill="outline" title="Modify">
-                        <IonIcon slot="icon-only" icon={create}></IonIcon>
-                      </IonButton>
-                      <IonButton color="primary" fill="outline" title="Swap">
-                        <IonIcon slot="icon-only" icon={swapHorizontal}></IonIcon>
-                      </IonButton>
-                    </IonCard>
-                  </IonCol>
-                </IonRow>
-              )) :
+              data.map((datum, i) => {
+                const { originalText, tags, shouldReplaceTextsWithTags, isEditing, hasApproved } = datum;
+                return (
+                  <IonRow key={i}>
+                    <IonCol>
+                      <IonCard className={styles.card}>
+                        <IonCardContent>
+                          <s-magic-text ref={el => {
+                            if (el) {
+                              el.text = originalText;
+                              el.highlights = tags?.map(tag => ({
+                                ...tag,
+                                tag: tag.name,
+                                style: { color: 'lightblue' }
+                              }));
+                              el.shouldReplaceTextWithTag = shouldReplaceTextsWithTags;
+                              const clickHandler = (event: Event) => {
+                                const detail = (event as CustomEvent).detail;
+                                const tagName = prompt('Given a tag for this (put nothing to remove the tag):', '');
+                                const tag = tags.find(tag => tag.start === detail.start && tag.end === detail.end);
+                                if (tagName) {
+                                  if (tag) {
+                                    tag.name = tagName;
+                                  } else {
+                                    tags.push({
+                                      start: detail.start,
+                                      end: detail.end,
+                                      name: tagName
+                                    });
+                                  }
+                                } else {
+                                  datum.tags = tags.filter(filteringTag => filteringTag !== tag);
+                                }
+                                forceUpdate({});
+                              };
+                              if (isEditing) {
+                                el.segmentHoverStyle = { background: 'orange' };
+                                el.removeEventListener('segmentClick', datum.clickHandler);
+                                el.addEventListener('segmentClick', clickHandler);
+                                datum.clickHandler = clickHandler;
+                              } else {
+                                el.segmentHoverStyle = {};
+                                el.removeEventListener('segmentClick', datum.clickHandler);
+                              }
+                            }
+                          }}></s-magic-text>
+                        </IonCardContent>
+                      </IonCard>
+                    </IonCol>
+                    <IonCol size="auto">
+                      <IonCard className={styles.card} style={{ width: '192px' }}>
+                        <IonButton
+                          color="primary"
+                          fill={shouldReplaceTextsWithTags ? 'solid' : 'clear'}
+                          title="Swap"
+                          onClick={() => {
+                            datum.shouldReplaceTextsWithTags = !shouldReplaceTextsWithTags;
+                            forceUpdate({});
+                          }}
+                        >
+                          <IonIcon slot="icon-only" icon={swapHorizontal}></IonIcon>
+                        </IonButton>
+                        <IonButton
+                          disabled={hasApproved}
+                          color="warning"
+                          fill={isEditing ? 'solid' : 'clear'}
+                          title="Modify"
+                          onClick={() => {
+                            datum.isEditing = !isEditing;
+                            forceUpdate({});
+                          }}
+                        >
+                          <IonIcon slot="icon-only" icon={create}></IonIcon>
+                        </IonButton>
+                        <IonButton
+                          disabled={hasApproved}
+                          color="success"
+                          fill={hasApproved ? 'solid' : 'clear'}
+                          title="Approve"
+                          onClick={() => {
+                            datum.hasApproved = !hasApproved;
+                            datum.isEditing = false;
+                            forceUpdate({});
+                          }}
+                        >
+                          <IonIcon slot="icon-only" icon={checkmark}></IonIcon>
+                        </IonButton>
+                      </IonCard>
+                    </IonCol>
+                  </IonRow>
+                );
+              }) :
               'No Data Yet'
           }
         </IonGrid>
