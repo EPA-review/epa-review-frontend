03632b0027f54cdb76dc321099739a5f834b80ca
Sean Wong <seanwong24@live.com>
2021-09-16 12:31:02 -0600

    allow admins to delete epa groups

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -4,13 +4,24 @@ import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import MainMenu from '../components/MainMenu';
 import UserMenu from '../components/UserMenu';
+import { fetchUser } from '../utils/auth';
 import ServerInfo from '../utils/ServerInfo';
+import { User } from '../utils/User';
 
 const Review: React.FC = () => {
+  const [, forceUpdate] = useState({});
   const history = useHistory();
+  const [user, setUser] = useState<User>();
   const [groupTags, setGroupTags] = useState<string[]>();
   const [presentMainMenuPopover, dismissMainMenuPopover] = useIonPopover(MainMenu, { onHide: () => dismissMainMenuPopover() });
   const [presentUserMenuPopover, dismissUserMenuPopover] = useIonPopover(UserMenu, { onHide: () => dismissUserMenuPopover() });
+
+  useEffect(() => {
+    async function fetchAndSetUserInfo() {
+      setUser(await fetchUser());
+    }
+    fetchAndSetUserInfo();
+  }, [history]);
 
   useEffect(() => {
     async function obtainGroupTags() {
@@ -53,7 +64,32 @@ const Review: React.FC = () => {
         {
           groupTags ?
             groupTags.map(groupTag => (
-              <IonItem key={groupTag} button routerLink={`/review/${groupTag}`}>{groupTag}</IonItem>
+              <IonItem key={groupTag} button routerLink={`/review/${groupTag}`}>
+                {groupTag}
+                {
+                  user?.roleName === 'admin' &&
+                  <IonButton
+                    color="danger"
+                    slot="end"
+                    onClick={async () => {
+                      if (window.confirm(`Are you sure to remove ${groupTag}?`)) {
+                        const response = await fetch(
+                          `${ServerInfo.SERVER_BASE_URL}/epa?groupTag=${groupTag}`,
+                          {
+                            method: 'DELETE',
+                            credentials: 'include'
+                          }
+                        );
+                        if(response.ok) {
+                          forceUpdate({});
+                        } else {
+                          alert(`Fail to remove ${groupTag}.`)
+                        }
+                      }
+                    }}
+                  >Delete</IonButton>
+                }
+              </IonItem>
             )) :
             'Loading data...'
         }
