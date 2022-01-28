061700e70bad7082a0f8f25e2aa6b919ac1907f2
Sean Wong <seanwong24@live.com>
2022-01-28 15:48:51 -0600

    add try for fetchUser

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,47 +2,44 @@ import ServerInfo from "./ServerInfo";
 import { User } from "./User";
 
 export async function fetchUser() {
-  let response = await fetch(
-    `${ServerInfo.SERVER_BASE_URL}/user/me`,
-    { credentials: 'include' }
-  );
-  let user: User | undefined;
-  if (response.ok) {
-    user = await response.json() as User;
-    response = await fetch(
-      `${ServerInfo.SERVER_BASE_URL}/role/me`,
-      { credentials: 'include' }
-    );
-    user.roleName = (await response.json())['name'];
-  }
-  return user;
+  try {
+    let response = await fetch(`${ServerInfo.SERVER_BASE_URL}/user/me`, {
+      credentials: "include",
+    });
+    let user: User | undefined;
+    if (response.ok) {
+      user = (await response.json()) as User;
+      response = await fetch(`${ServerInfo.SERVER_BASE_URL}/role/me`, {
+        credentials: "include",
+      });
+      user.roleName = (await response.json())["name"];
+    }
+    return user;
+  } catch {}
 }
 
 export async function signIn(userInfo: User) {
   const response = await fetch(
     `${ServerInfo.SERVER_BASE_URL}/authentication/sign-in`,
     {
-      method: 'POST',
-      credentials: 'include',
+      method: "POST",
+      credentials: "include",
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
       body: JSON.stringify({
         username: userInfo.username,
-        authenticationHash: userInfo.authenticationHash
-      })
+        authenticationHash: userInfo.authenticationHash,
+      }),
     }
   );
   return response.ok;
 }
 
 export async function signOut() {
-  await fetch(
-    `${ServerInfo.SERVER_BASE_URL}/authentication/sign-out`,
-    {
-      method: 'POST',
-      credentials: 'include'
-    }
-  );
+  await fetch(`${ServerInfo.SERVER_BASE_URL}/authentication/sign-out`, {
+    method: "POST",
+    credentials: "include",
+  });
   window.location.reload();
 }
